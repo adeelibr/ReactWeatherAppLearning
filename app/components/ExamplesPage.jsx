138bb61505061303fdafb1d1bdab63d20106ee3f
adeelibr <adeelimranr@gmail.com>
2016-10-23 00:28:04 +0500

    Examples Page added

---
+++ app/components/ExamplesPage.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Link} from 'react-router';
+
+let ExamplesPage = () => {
+  return (
+    <div>
+      <h1 className="text-center">Examples</h1>
+      <p>Here are a few examples for you to try out.</p>
+      <ol>
+        <li><Link to="/?location=Lahore">Lahore, Pk</Link></li>
+        <li><Link to="/?location=Karachi">Karachi, Pk</Link></li>
+        <li><Link to="/?location=Miami">Miami, Fl</Link></li>
+      </ol>
+    </div>
+  );
+};
+
+export default ExamplesPage;
