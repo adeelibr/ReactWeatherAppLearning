a196fb38e288bb1e96b259b5cbfb939794c1d13a
adeelibr <adeelimranr@gmail.com>
2016-10-22 21:58:51 +0500

    Server file corrections

--- server.js
+++ server.js
@@ -14,5 +14,5 @@ app.use(function (req, res, next) {
 app.use(express.static('public'));
 
 app.listen(PORT, function() {
-  console.log('Express Server Is Running On Port ' + port);
+  console.log('Express Server Is Running On Port ' + PORT);
 });
